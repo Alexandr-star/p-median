ec2fa0ca3f4d8db7694117ebab6fc6480312de9a
Sasha <sanya-bass.skvortzoff@yandex.ru>
2020-06-23 20:26:36 +0300

    Revert "Рефакторинг AdjacencyList и Cost"
    This reverts commit 0c7b02b6efbb9edbacb0ad43f5a6b4682de61177.

--- Pmedian/Pmedian/CoreData/DataStruct/AdjacencyList.cs
+++ Pmedian/Pmedian/CoreData/DataStruct/AdjacencyList.cs
@@ -21,13 +21,28 @@ namespace Pmedian.CoreData.DataStruct
         /// Список смежности графа.
         /// </summary>
         public List<List<int>> adjacencyList = new List<List<int>>();
-     
+
+        
+
         static public Cost cost { get; set; }
         
+        private List<List<int>> typeListVertex = new List<List<int>>();
+
         /// <summary>
         /// Количество вершин в графе.
         /// </summary>
         public int VertexCount => adjacencyList.Count;
+
+        /// <summary>
+        /// Максимальное количество вершин в списке смежности.
+        /// </summary>
+        // TODO: ошибка вылетает, надо исправить
+        public int VertexCountInMaxList => MaxCountListInAdj();
+
+        /// <summary>
+        /// Связность графа.
+        /// </summary>
+        public bool IsConnected => CheckConnectivity();
 
         /// <summary>
         /// Ориентированность графа.
@@ -83,7 +98,15 @@ namespace Pmedian.CoreData.DataStruct
         {
             return adjacencyList[source].Contains(target);
         }
-       
+
+        /// <summary>
+        /// Возращает список с деревнями
+        /// </summary>
+        /// <returns></returns>
+        public List<int> GetVillageList()
+        {
+            return typeListVertex[0];
+        }
 
         /// <summary>
         /// Список смежных к исходной вершин.
@@ -102,12 +125,65 @@ namespace Pmedian.CoreData.DataStruct
         private void InitializeList(int vertexCount)
         {
             adjacencyList = new List<List<int>>();
+            typeListVertex = new List<List<int>>();
             for (int i = 0; i < vertexCount; i++)
             {
                 adjacencyList.Add(new List<int>());
-            }                        
+            }
+            
+            for (int i = 0; i < 3; i++)
+                typeListVertex.Add(new List<int>());
         }
-                    
+
+        /// <summary>
+        /// Добавить вершину в список вершинын распределенных по типу.
+        /// </summary>
+        /// <param name="type">Тип вершины.</param>
+        /// <param name="vertex">Вершина.</param>
+        private void AddVertexInTypeList(int type, int vertex)
+        {
+            typeListVertex[type].Add(vertex);
+        }
+        
+        private int MaxCountListInAdj()
+        {
+            List<int> countList = new List<int>();
+            foreach(var list in adjacencyList)
+            {
+                countList.Add(list.Count);
+            }
+            return countList.Max();
+        }
+
+        /// <summary>
+        /// Проверка связности графа. Истинна, если граф является связным.
+        /// </summary>
+        /// <returns>Истина, если граф является связным.</returns>
+        /// TODO: Сделать нормальное преобразование в граф.
+        private bool CheckConnectivity()
+        {
+            var nodes = new List<int>(VertexCount);
+            var visited = Enumerable.Repeat(false, VertexCount).ToArray();
+
+            nodes.Add(0);
+            while (nodes.Count > 0)
+            {
+                var curr = nodes[0]; nodes.RemoveAt(0);
+
+                foreach (var val in GetAdjacent(curr))
+                    if (visited[val] == false)
+                        nodes.Add(val);
+
+                visited[curr] = true;
+            }
+
+            foreach (var val in visited)
+                if (val == false)
+                    return false;
+
+            return true;
+        }
+
         /// <summary>
         /// Создание нового экземлпяра списка смежности на основе указанного графа.
         /// </summary>
@@ -125,9 +201,35 @@ namespace Pmedian.CoreData.DataStruct
                 int target = vertices.IndexOf(edge.Target);
                 list.AddEdge(source, target);
             }
+            PrintGraph(list);
             return list;
         }
+
        
+        public static AdjacencyList GenerateList(Chromosome chromosome, Cost costt)
+        {
+            if (chromosome == null)
+                return null;
+            cost = costt;
+            var list = new AdjacencyList(cost.vertexCount);
+            var costList = cost.CostList;
+            var chromArray = chromosome.chromosomeArray;
+            int c = 0;
+            for (int i = 0; i < list.VertexCount; i++)
+            {
+                if (costList[i].Count == 0) continue;
+                for (int j = 0, ich = c; j < costList[i].Count; j++, c++, ich++)
+                {
+                    if (costList[i][j] * chromArray[ich] != 0)
+                    {
+                        list.AddEdge(i, costList[i][j]);
+                    }
+                }
+            }
+            AdjacencyList.PrintGraph(list);
+            return list;
+        }
+
         /// <summary>
         /// Создание нового экземлпяра графа на основе указанного списка смежности. 
         /// </summary>
@@ -137,6 +239,7 @@ namespace Pmedian.CoreData.DataStruct
         {
             if (list == null)
                 return null;
+            AdjacencyList.PrintGraph(list);
             MainGraph graph = new MainGraph();
 
             for (int i = 0; i < villCount; i++)
@@ -160,6 +263,24 @@ namespace Pmedian.CoreData.DataStruct
             }
 
             return graph;
-        }              
+        }
+        
+        public static void PrintGraph(AdjacencyList list)
+        {
+            Console.WriteLine("print adjacency list");
+            for (int i = 0; i < list.VertexCount; i++)
+            {
+                Console.Write($"{i} - ");
+                foreach (int j in list.adjacencyList[i])
+                {
+                    Console.Write(j);
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }           
+        }
+
+        
+
     }
 }
